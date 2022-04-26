c4f9b187e897e2988e5e199b29c3634a95ca6e73
Usama Ansari <student9970@gmail.com>
2022-04-26 20:12:39 +0530

    ADD: Section for the Tesla

---
+++ src/components/Section.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Section(props) {
+    return (
+        <Wrap bgImage={props.backgroundImg}>
+
+            <ItemText>
+                <h1>{props.title}</h1>
+                <p>{props.description}</p>
+            </ItemText>
+
+            <Buttons>
+                <ButtonGroup>
+
+                    <LeftButton>
+                        {props.leftBtnText}
+                    </LeftButton>
+
+                    {
+                        props.rightBtnText &&
+
+                        // If there is rightBtnText present then only show the right button
+
+                        <RightButton>
+                            {props.rightBtnText}
+                        </RightButton>
+                    }
+
+                </ButtonGroup>
+
+                <DownArrow src="/images/down-arrow.svg" alt="down arrow" />
+            </Buttons>
+
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: url('/images/model-s.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background-image: ${props => `url("/images/${props.bgImage}")`};
+`
+
+const ItemText = styled.div`
+      padding-top: 15vh;
+      text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background-color: #fff;
+    opacity: 0.65;
+    color: #000;
+`
+
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
+    overflow: hidden;
+    animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div`
+
+`
